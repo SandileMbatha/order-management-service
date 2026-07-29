aab9d348521bff7e3966744ede4c105e188056d7
SandileMbatha <sandile7003@gmail.com>
2026-07-30 00:01:32 +0200

    Add Swagger/OpenAPI documentation
    - Add springdoc-openapi dependency for auto-generated docs
    - Configure OpenAPI with API key security scheme
    - Swagger UI available at /swagger-ui.html
    - API docs available at /v3/api-docs

---
+++ src/main/java/com/pollinate/ordermanagement/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package com.pollinate.ordermanagement.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Order Management API")
+                        .description("REST API for managing Products and Orders")
+                        .version("1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList("ApiKeyAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("ApiKeyAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.APIKEY)
+                                .in(SecurityScheme.In.HEADER)
+                                .name("X-API-Key")));
+    }
+}
